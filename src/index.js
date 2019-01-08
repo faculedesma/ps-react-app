f82a860b928544cee52a06e4fb4a5c40cbf39c57
unknown <faculedesma1993@gmail.com>
2019-01-08 17:30:19 -0300

    Intro to Redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hola');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import router from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import routes from './routes';
+
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
